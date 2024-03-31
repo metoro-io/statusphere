2e7baee9182bfab161afe683165f5e1a01cb8c5f
Chris Battarbee <chris@metoro.io>
2024-03-31 14:30:35 +0100

    Remove i18n

--- statusphere-frontend/next.config.mjs
+++ statusphere-frontend/next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/statusphere',
-    i18n: {
-        locales: ['en'],
-        defaultLocale: 'en',
-    }
 };
 
 export default nextConfig;
