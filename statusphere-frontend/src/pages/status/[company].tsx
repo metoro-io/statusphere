5529f33dc644f034a6b9c411c320313dbe35a08e
Chris Battarbee <Chrisbattarbee@users.noreply.github.com>
2024-04-02 11:45:36 +0100

    Add show api call button (#23)

--- statusphere-frontend/src/pages/status/[company].tsx
+++ statusphere-frontend/src/pages/status/[company].tsx
@@ -5,6 +5,16 @@ import {CurrentStatus} from "@/components/CurrentStatus";
 import {Outages} from "@/components/Outages";
 import {RecommendCompany} from "@/components/RecommendCompany";
 import Head from "next/head";
+import React from "react";
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger
+} from "@/components/ui/drawer";
+import {CopyBlock, dracula} from "react-code-blocks";
 
 interface CompanyStatusPageProps {
     statusPageDetails: StatusPage
@@ -56,12 +66,50 @@ export default function CompanyStatusPage({
                                               outages,
                                               isError,
                                           }: CompanyStatusPageProps) {
+
+    const apiCallCurrentStatus = `curl -X GET "https://statusphere.metoro.io/api/v1/currentStatus?statusPageUrl=${statusPageDetails.url}"`
+    const apiCallIncidents = `curl -X GET "https://statusphere.metoro.io/api/v1/incidents?statusPageUrl=${statusPageDetails.url}"`
+
+
     return (
         <div>
-            <div className={"flex justify-center w-full"}>
-                <h1 className={"text-2xl"}>
-                    {companyName} Status
-                </h1>
+            <div className={"flex justify-center px-[10vw]"}>
+                <div className={"flex justify-start w-full"}>
+                    <h1 className={"text-2xl"}>
+                        {companyName} Status
+                    </h1>
+                </div>
+                <div className={"flex justify-end w-full"}>
+                    <Drawer>
+                        <DrawerTrigger asChild>
+                            <button
+                                className={"text-sm text-gray-500"}>
+                                Show API Calls
+                            </button>
+                        </DrawerTrigger>
+                        <DrawerContent className={"bg-white"}>
+                            <div className="mx-auto w-full max-w-xl">
+                                <DrawerHeader>
+                                    <DrawerTitle className={"text-2xl"}>API calls</DrawerTitle>
+                                    <DrawerDescription>Programmatically
+                                        get {statusPageDetails.name} status.</DrawerDescription>
+                                </DrawerHeader>
+                                {/*The current status api call*/}
+                                <div>
+                                    <div className={"text-lg font-semibold mb-4"}> Get current status</div>
+                                    <CopyBlock language={"bash"} text={apiCallCurrentStatus} showLineNumbers={false}
+                                               wrapLongLines={true} theme={dracula}/>
+                                </div>
+                                {/*The incidents api call*/}
+                                <div className={"mb-4"}>
+                                    <div className={"text-lg font-semibold mb-4 mt-4"}> Get historical incidents</div>
+                                    <CopyBlock language={"bash"} text={apiCallIncidents} showLineNumbers={false}
+                                               wrapLongLines={true} theme={dracula}/>
+                                </div>
+                            </div>
+                        </DrawerContent>
+                    </Drawer>
+                </div>
             </div>
             <div className={"mt-4 flex justify-center w-full z-10"}>
                 <Head>
